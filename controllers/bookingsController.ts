079fdd7a1e33400dd6ee581c1ffa22b9c36234d5
casandrabenpor <casandra211@gmail.com>
2023-07-04 12:38:42 +0200

    crud bookings

--- controllers/bookingsController.ts
+++ controllers/bookingsController.ts
@@ -10,13 +10,14 @@ import bodyParser from 'body-parser';
 
 export const bookingsController = Router();
 
-bookingsController.get('/', (req, res) => {
-  res.status(200).json(getBooking());
+bookingsController.get('/', async (req, res) => {
+  let bookings = await getBooking();
+  res.status(200).json(bookings);
 });
 
-bookingsController.get('/:id', (req, res) => {
+bookingsController.get('/:id', async (req, res) => {
   const bookingId = req.params.id;
-  const booking = getById(bookingId);
+  const booking = await getById(bookingId);
   if (booking !== null) {
     res.status(200).json(booking);
   } else {
@@ -24,15 +25,15 @@ bookingsController.get('/:id', (req, res) => {
   }
 });
 
-bookingsController.post('/', bodyParser.json(), (req, res) => {
+bookingsController.post('/', bodyParser.json(), async (req, res) => {
   res.status(200).json(addBooking(req.body));
 });
 
-bookingsController.put('/', bodyParser.json(), (req, res) => {
+bookingsController.put('/', bodyParser.json(), async (req, res) => {
   res.status(200).json(updateBooking(req.body));
 });
 
-bookingsController.delete('/', (req, res) => {
+bookingsController.delete('/', async (req, res) => {
   let id = req.query.id as string;
   res.status(200).json(deleteBooking(id));
 });
--- services/bookingsService.ts
+++ services/bookingsService.ts
@@ -1,38 +1,105 @@
-import bookingsData from '../data/bookings.json';
+import mysql from 'mysql2/promise';
 import { Booking } from '../models/interface';
-import { saveToDataBase } from './dataBaseService';
 
-export const getBooking = () => {
-  return bookingsData;
+export const getBooking = async () => {
+  const query =
+    'SELECT id,guest,order_date,check_in,check_in_hour,check_out,check_out_hour,room_type,room_number,status,room_id;';
+
+  let connection = await mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: process.env.DB_PASSWORD,
+    database: 'hotel_miranda',
+  });
+
+  const [rows] = await connection.execute(query);
+
+  await connection.end();
+  return rows;
 };
-export const getById = (bookingId: string) => {
-  const booking = bookingsData.find((b) => b.id === bookingId) || null;
-  return booking;
+export const getById = async (bookingId: string) => {
+  const query =
+    'SELECT id,guest,order_date,check_in,check_in_hour,check_out,check_out_hour,room_type,room_number,status,room_id from bookings WHERE id = ?;';
+  const params = [bookingId];
+
+  let connection = await mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: process.env.DB_PASSWORD,
+    database: 'hotel_miranda',
+  });
+
+  const [rows] = await connection.execute(query, params);
+
+  let users = rows as any[];
+
+  await connection.end();
+
+  return users[0];
 };
 
-export const addBooking = (booking: Booking) => {
-  bookingsData.push(booking);
-  saveToDataBase(bookingsData, 'bookings.json');
+export const addBooking = async (booking: Booking) => {
+  const query =
+    'INSERT INTO bookings (guest,order_date,check_in,check_in_hour,check_out,check_out_hour,room_type,room_number,status,room_id) VALUES (?,?,?,?,?,?,?,?,?,?)';
+  const params = [
+    booking.guest,
+    booking.order_date,
+    booking.check_in,
+    booking.check_in_hour,
+    booking.check_out,
+    booking.check_out_hour,
+    booking.room_type,
+    booking.room_number,
+    booking.status,
+    booking.room_id,
+  ];
+  let connection = await mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: process.env.DB_PASSWORD,
+    database: 'hotel_miranda',
+  });
+  await connection.execute(query, params);
+  console.log(addBooking);
 };
 
-export const updateBooking = (booking: Booking) => {
-  const existingBooking = bookingsData.find((b) => b.id === booking.id);
+export const updateBooking = async (booking: Booking) => {
+  const query =
+    'UPDATE bookings ' +
+    'SET guest = ?, order_date = ?, check_in = ?, check_in_hour = ?, check_out = ?, check_out_hour = ?, room_type = ?, room_number = ? ,status = ?, room_id = ? WHERE id = ? ';
+  const params = [
+    booking.guest,
+    booking.order_date,
+    booking.check_in,
+    booking.check_in_hour,
+    booking.check_out,
+    booking.check_out_hour,
+    booking.room_type,
+    booking.room_number,
+    booking.status,
+    booking.room_id,
+    booking.id,
+  ];
+  let connection = await mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: process.env.DB_PASSWORD,
+    database: 'hotel_miranda',
+  });
 
-  if (existingBooking) {
-    // Crear una copia de la reserva existente sin modificar el ID
-    const updatedBooking: Booking = {
-      ...booking,
-      id: existingBooking.id ? existingBooking.id.toString() : '',
-    };
-
-    let index = bookingsData.findIndex((b) => b.id === booking.id);
-    bookingsData[index] = updatedBooking;
-    saveToDataBase(bookingsData, 'bookings.json');
-  }
+  await connection.execute(query, params);
 };
 
-export const deleteBooking = (id: string) => {
-  let index = bookingsData.findIndex((b) => b.id === id);
-  let filterBookings = bookingsData.filter((b) => b.id != id);
-  saveToDataBase(filterBookings, 'bookings.json');
+export const deleteBooking = async (id: string) => {
+  const query = 'DELETE FROM bookings WHERE id = ?';
+  const params = [id];
+
+  let connection = await mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: process.env.DB_PASSWORD,
+    database: 'hotel_miranda',
+  });
+
+  await connection.execute(query, params);
 };
