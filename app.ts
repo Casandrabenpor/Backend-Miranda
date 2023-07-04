2ed3141fd5bab264fb43cb18d860886af4bb87d8
casandrabenpor <casandra211@gmail.com>
2023-07-04 21:20:01 +0200

    aws deploy

---
+++ app.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import { usersController } from './controllers/usersController';
+import { bookingsController } from './controllers/bookingsController';
+import { roomsController } from './controllers/roomsController';
+import { verifyTokenMiddleware } from './middleware/auth';
+import { contactController } from './controllers/contactController';
+import { authenticationController } from './controllers/authController';
+import { infoController } from './controllers/infoController';
+require('dotenv').config();
+
+export const app = express();
+
+//public routes
+app.use('/login', authenticationController);
+app.use('/info', infoController);
+//private routes
+app.use('/users', verifyTokenMiddleware, usersController);
+app.use('/bookings', verifyTokenMiddleware, bookingsController);
+app.use('/rooms', verifyTokenMiddleware, roomsController);
+app.use('/contact', verifyTokenMiddleware, contactController);
+
+export default app;
--- dist/tests/routes.test.js
+++ dist/tests/routes.test.js
@@ -3,7 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = __importDefault(require("../util/app"));
+const app_1 = __importDefault(require("../app"));
 const supertest_1 = __importDefault(require("supertest"));
 describe('GET /info', () => {
     it('returns status code 200 if first name is passed', async () => {
